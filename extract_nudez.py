b90393ab30200590503956090f16930b65c273f8
Pax Humana <paxhumana@protonmail.com>
2022-11-18 09:35:56 -0800

    Ported to Tensorflow2 OpenNSFW

--- extract_nudez.py
+++ extract_nudez.py
@@ -2,7 +2,6 @@
 
 import argparse
 import string
-import random
 import os
 import sys
 import numpy
@@ -15,10 +14,15 @@ import opennsfw2
 NSFW_MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--threshold', type=float, default=.7, help='Default matching threshold for the open_nsfw model (0 - 1). Default: .7"')
 parser.add_argument('directory', nargs='?', type=str, default='./', help='Input Directory. Default: current working directory.')
+parser.add_argument('output', nargs='?', type=str, default='wins', help='Output Subdirectory. Default: wins')
 args = parser.parse_args()
 
+SCORE_THRESHOLD = args.threshold
 INPUT_DIR = os.path.abspath(args.directory)
+OUTPUT_DIR = os.path.abspath(os.path.join(INPUT_DIR, args.output))
+EXCLUDE = set([args.output])
 
 def resize_image(data, sz=(256, 256)):
     im = Image.open(BytesIO(data))
@@ -36,17 +40,25 @@ def preprocess_and_compute(pimg, model):
     return model.predict(inputs)
 
 def main():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
     # Pre-load Tensorflow OpenNSFW2 model
     model = opennsfw2.make_open_nsfw_model()
 
-    for filename in os.listdir(INPUT_DIR):
-        if not filename.startswith("."):
-            if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".gif"):
-                image_data = open(os.path.join(INPUT_DIR, filename), 'rb').read()
-#                with open(os.path.join(INPUT_DIR, filename), 'rb') as f:
-#                    image_data = f.read()
-                scores = preprocess_and_compute(image_data, model)
-                print(filename, "NSFW score:" , scores[0][1])
+    for fulldir, subdir, files in os.walk(INPUT_DIR):
+        subdir[:] = [d for d in subdir if d not in EXCLUDE] # Exclude the output directory
+        for filename in files:
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.tif', '.bmp', '.gif')):
+                try:
+                    image_data = open(os.path.join(fulldir, filename), 'rb').read()
+                    scores = preprocess_and_compute(image_data, model)
+                except:
+                    continue
+                if scores[0][1] >= SCORE_THRESHOLD:
+                    print(os.path.join(fulldir, filename), "NSFW score:" , scores[0][1])
+                    os.rename(os.path.join(fulldir, filename), os.path.join(OUTPUT_DIR, filename))
+            
             else:
                 print(os.path.join(INPUT_DIR, filename), "is not a supported image.")
 
--- pls_send_nudez.py
+++ pls_send_nudez.py
@@ -8,18 +8,16 @@ import hashlib
 import os
 import sys
 import numpy
-import bs4
 from PIL import Image
 from io import BytesIO
 
-os.environ['GLOG_minloglevel'] = '3' # supress Caffe verbose prints
-import caffe
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Suppress Tensorflow log messages
+import opennsfw2
 
 # Defaults
 IMAGE_SIZE_MIN = 1024 * 20 # Minimum filesize (20k) for downloaded images.
 IMGUR_URL_HASH_LENGTH = 5 # Default imgur URL hash length. Should be 5 or 7.
 NSFW_MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
-# GPU_ID = 0
 
 # Constants
 IMAGE_EXTENSION = ".jpg" # Extension for search.
@@ -29,7 +27,6 @@ IMGUR_URL_PREFIX = "http://i.imgur.com/" # Prefix for Imgur URLs.
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--min-size', type=int, default=IMAGE_SIZE_MIN, help='Minimum image size in kilobytes. (Default: ' + str(IMAGE_SIZE_MIN) + ')')
 parser.add_argument('-7', '--new-hash-length', action='store_true', help='Use image filename length of 7. (Default is 5)')
-parser.add_argument('-n', '--nsfw-model', type=str, default=NSFW_MODEL_DIR, help='Location of the open_nsfw model root directory. Default: ' + str(NSFW_MODEL_DIR) + ')')
 parser.add_argument("output", type=str, help='Output folder name.')
 args = parser.parse_args()
 
@@ -39,13 +36,13 @@ if args.min_size is not IMAGE_SIZE_MIN:
 if args.new_hash_length is True:
     IMGUR_URL_HASH_LENGTH = 7
 
-NSFW_MODEL_DIR = os.path.abspath(args.nsfw_model)
 OUTPUT_DIR = os.path.abspath(args.output)
 
 # Functions
 def gen_random_img_hash(length):
     return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length))
     #return "4iboqds" # A nice pussy for testing
+    #return "BgzZmlo" # An image to train on
 
 def get_image(image_name):
     image_url = IMGUR_URL_PREFIX + image_name
@@ -66,69 +63,38 @@ def resize_image(data, sz=(256, 256)):
     fh_im.seek(0)
     return fh_im
 
-def caffe_preprocess_and_compute(pimg, caffe_transformer=None, caffe_net=None, output_layers=None):
-    if caffe_net is not None:
-
-        # Grab the default output names if none were requested specifically.
-        if output_layers is None:
-            output_layers = caffe_net.outputs
-
-        img_bytes = resize_image(pimg, sz=(256, 256))
-        image = caffe.io.load_image(img_bytes)
-
-        H, W, _ = image.shape
-        _, _, h, w = caffe_net.blobs['data'].data.shape
-        h_off = max((H - h) / 2, 0)
-        w_off = max((W - w) / 2, 0)
-        crop = image[int(h_off):int(h_off + h), int(w_off):int(w_off + w), :]
-        transformed_image = caffe_transformer.preprocess('data', crop)
-        transformed_image.shape = (1,) + transformed_image.shape
-
-        input_name = caffe_net.inputs[0]
-        all_outputs = caffe_net.forward_all(blobs=output_layers,
-            **{input_name: transformed_image})
-
-        outputs = all_outputs[output_layers[0]][0].astype(float)
-        return outputs
-    else:
-        return []
+def preprocess_and_compute(pimg, model):
+    image = opennsfw2.preprocess_image(Image.open(BytesIO(pimg)), opennsfw2.Preprocessing.YAHOO)
+    inputs = numpy.expand_dims(image, axis=0)
+    return model.predict(inputs)
 
 # Main
 def main():
-    # Pre-load open_nsfw caffe model from script directory
-    nsfw_net = caffe.Net(os.path.join(os.sep, NSFW_MODEL_DIR, "nsfw_model", "deploy.prototxt"),
-        os.path.join(os.sep, NSFW_MODEL_DIR, "nsfw_model", "resnet_50_1by2_nsfw.caffemodel"),
-        caffe.TEST)
+    # Pre-load Tensorflow OpenNSFW2 model
+    model = opennsfw2.make_open_nsfw_model()
 
-    # Load transformer
-    # Note that the parameters are hard-coded for best results
-    # caffe.set_device(GPU_ID)
-    # caffe.set_mode_gpu()
-    caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
-    caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
-    caffe_transformer.set_mean('data', numpy.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
-    caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
-    caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
+    # Create output directory
+    if not os.path.isdir(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+    # Find images and apply the model
     while True:
         # Get a random image
         image_data = None
         while image_data is None:
-            # image_name = gen_random_img_hash(random.choice([5, 7])) + ".jpg"
             image_name = gen_random_img_hash(IMGUR_URL_HASH_LENGTH) + IMAGE_EXTENSION
             image_data = get_image(image_name)
         image_path = os.path.join(os.sep, OUTPUT_DIR, image_name)
-        # print("TRY: " + image_name)
 
         # Classify downloaded image
         try:
-            scores = caffe_preprocess_and_compute(image_data, caffe_transformer=caffe_transformer, caffe_net=nsfw_net, output_layers=['prob'])
+            scores = preprocess_and_compute(image_data, model) 
         except:
             print("ERR! " + image_name)
             continue
 
         # Save wins
-        if scores[1] >= .7:
+        if scores[0][1] >= .7:
             print("WIN! " + image_name)
             with open(image_path, "wb") as f:
                 f.write(image_data)
